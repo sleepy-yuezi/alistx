09d6db98419689d15c6637b81456520f37a959cc
sleepy-yuezi <104213398+sleepy-yuezi@users.noreply.github.com>
2023-03-30 01:09:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#测试volume
+mkdir -p /root/.aria2
+cd /root/.aria2
+wget https://github.com/P3TERX/aria2.conf/archive/refs/heads/master.tar.gz
+tar -zxvf master.tar.gz --strip-components=1
+rm -rf master.tar.gz
+sed -i 's|rpc-secret|#rpc-secret|g' ./aria2.conf
+touch /root/.aria2/aria2.session
+./tracker.sh
+
+
 chown -R ${PUID}:${PGID} /opt/alist/
 
 umask ${UMASK}
